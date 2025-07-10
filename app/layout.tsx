c0606ab0db81fb01973d9db09aacd92f07d06aef
RickyIG <rickyindra53@gmail.com>
2025-07-10 22:51:48 +0700

    Menambahkan file proyek awal saya

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+ 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"></link>
+      </head>
+      {/* <body style={{ fontFamily: 'Roboto' }}>{children}</body> */}
+      <body className={`${inter.className} antialiased}`}>{children}</body>
+    </html>
+  );
+}
