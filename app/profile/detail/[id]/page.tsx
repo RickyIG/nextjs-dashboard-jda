b653c431542f49d2d6601d3a1cd4373d46c0a48b
RickyIG <rickyindra53@gmail.com>
2025-07-19 22:30:23 +0700

    Fix

--- app/profile/detail/[id]/page.tsx
+++ app/profile/detail/[id]/page.tsx
@@ -3,8 +3,8 @@
 import { use, useState } from 'react';
 import Link from 'next/link';
 
-export default function Page({ params }: { params: Promise<{ id: string }> }) {
-    const { id } = use(params);
+export default async function Page({ params }: { params: Promise<{ id: string }> }) {
+    const { id } = await params;
     const [count, setCount] = useState(0);
 
     return (
